a0f0c66e65b87a03c08a506d8ebef704f7a73aac
James Berry <git@jamesberry.uk>
2021-02-07 13:55:28 +0000

    - Set up OpenGL and rendered basic triangle

--- COMP220-Code-Examples/main.cpp
+++ COMP220-Code-Examples/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <SDL.h>
+#include <gl\glew.h>
+#include <SDL_opengl.h>
 
-int main(int argc, char ** argsv)
+int main(int argc, char** argsv)
 {
 	//Initialises the SDL Library, passing in SDL_INIT_VIDEO to only initialise the video subsystems
 	//https://wiki.libsdl.org/SDL_Init
@@ -15,7 +17,7 @@ int main(int argc, char ** argsv)
 
 	//Create a window, note we have to free the pointer returned using the DestroyWindow Function
 	//https://wiki.libsdl.org/SDL_CreateWindow
-	SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_OPENGL);
 	//Checks to see if the window has been created, the pointer will have a value of some kind
 	if (window == nullptr)
 	{
@@ -27,7 +29,44 @@ int main(int argc, char ** argsv)
 		return 1;
 	}
 
-	
+	// Creating OpenGL context
+	SDL_GLContext glContext = SDL_GL_CreateContext(window);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	// Initalising GLEW
+	glewExperimental = GL_TRUE;
+	GLenum glewError = glewInit();
+	if (glewError != GLEW_OK)
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Unable to initialise GLEW", (char*)glewGetErrorString(glewError), NULL);
+	}
+
+	// Creating and setting the Vertex Array Object
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// 3 Vectors representing 3 vertices as an array
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+	};
+
+	// Creating a buffer
+
+	// Identifies vertex buffer
+	GLuint vertexbuffer;
+	// Generates 1 buffer and puts it in the vertex buffer
+	glGenBuffers(1, &vertexbuffer);
+	// Talks about vertexbuffer (????)
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// Gives vertices to OpenGL
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+
 	//Event loop, we will loop until running is set to false, usually if escape has been pressed or window is closed
 	bool running = true;
 	//SDL Event structure, this will be checked in the while loop
@@ -58,6 +97,23 @@ int main(int argc, char ** argsv)
 			}
 		}
 
+		// 1st attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,			// attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,			// size
+			GL_FLOAT,	// type
+			GL_FALSE,	// normalized?
+			0,			// stride
+			(void*)0	// array buffer offset
+		);
+
+		// Draw Triangle
+		glDrawArrays(GL_TRIANGLES,
+			0,		// Starting from vertex 0
+			3);	// 3 Total vertices
+		glDisableVertexAttribArray(0);
 
 		SDL_GL_SwapWindow(window);
 	}
